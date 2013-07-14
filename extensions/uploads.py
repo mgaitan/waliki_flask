f95be7d0cd1c80aef654833a9c0dbe744af283e6
Martín Gaitán <mgaitan@machinalis.com>
2013-07-14 13:31:04 -0300

    degradate to  standard POST in a popup window if browser can't upload files via ajax. Fuck you IE

--- extensions/uploads.py
+++ extensions/uploads.py
@@ -3,6 +3,17 @@ from flaskext.uploads import UploadSet, ALL, configure_uploads
 from flask import (render_template, flash, request, Blueprint, current_app,
                    abort, send_file, url_for)
 
+CLOSE_WINDOW_HTML = """
+<html>
+<head>
+<script type="text/javascript">
+    window.close();
+</script>
+</head>
+<body>
+</body>
+</html>"""
+
 
 def default_dest(app):
     return os.path.join(app.config.get('CONTENT_DIR'), 'uploads')
@@ -31,6 +42,10 @@ def upload(url):
     page = current_app.wiki.get_or_404(url)
     if request.method == 'POST' and 'attach' in request.files:
         media.save(request.files['attach'], folder=page.url)
+        if not request.is_xhr:
+            # Internet Explorer upload window is not ajaxified but
+            # popup. Just close it.
+            return CLOSE_WINDOW_HTML
     try:
         files = os.listdir(os.path.join(current_app.config.get('CONTENT_DIR'),
                                     'uploads', page.url))
@@ -58,8 +73,12 @@ def get_file(url, filename):
     outfile = _base_file(url, filename)
     return send_file(outfile)
 
+
 @uploads.route('/<path:url>/_remove/<filename>')
 def remove_file(url, filename):
     outfile = _base_file(url, filename)
     try:
         os.remove(outfile)
+    except:
+        pass
+    return ''

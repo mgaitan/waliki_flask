8c36096402463f4a5d2e953b04ccd8b1ffcfc042
JuanBC <jbc.develop@gmail.com>
2014-02-01 14:33:01 -0300

    add ut8 heads

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from waliki.app import app
 
 if __name__ == '__main__':
--- waliki/signals.py
+++ waliki/signals.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask.signals import Namespace
 
 wiki_signals = Namespace()
